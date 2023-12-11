37dec4425cf708ba60f36b8c55f3ece6c0e1efab
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-12-11 14:57:23 +0900

    Order페이지 UI 반영 및 Type 컴포넌트 프롭스에 따른 ui 출력 반영

--- client/src/pages/SummaryPage/index.js
+++ client/src/pages/SummaryPage/index.js
@@ -1,33 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const SummaryPage = () => {
-
-    const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
   return (
     <div>
       <form>
-        <input 
+        <input
           type="checkbox"
           checked={checked}
           id="confirm-checkbox"
-          onClick={(e)=>setChecked(e.target.checked)}
+          onClick={(e) => setChecked(e.target.checked)}
         />{" "}
-        <label htmlFor='confirm-checkbox'>
-          주문하려는 것을 확인하셨나요?
-        </label>
-        <br/>
-        <button 
-          type="submit"
-          disabled={!checked}  
-          
-        >
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button type="submit" disabled={!checked}>
           주문 확인
         </button>
       </form>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default SummaryPage
+export default SummaryPage;
