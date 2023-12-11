4afb554451f80df7aae220450f4a5b9fad5cf701
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-12-12 00:27:35 +0900

    에러배너 컴포넌트 반영

--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Products from "./Products";
 import Options from "./Options";
+import ErrorBanner from "./ErrorBanner";
 
 const Type = ({ orderType }) => {
   const [items, setItems] = useState([]);
-
+  const [error, setError] = useState(false);
   useEffect(() => {
     loadItems(orderType);
   }, [orderType]);
@@ -13,22 +14,27 @@ const Type = ({ orderType }) => {
   const loadItems = async (orderType) => {
     try {
       const response = await axios.get(`http://localhost:4000/${orderType}`);
-      setItems(response.data); 
+      setItems(response.data);
     } catch (error) {
-      console.log(error);
+      setError(true);
     }
   };
 
   const ItemComponent = orderType === "products" ? Products : Options;
 
-
-  const optionItems = items.map(item => (
-    <ItemComponent 
+  const optionItems = items.map((item) => (
+    <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
     />
-    ))
+  ));
+  
+  if(error) {
+    return(
+      <ErrorBanner />
+    )
+  }
 
   return (
     <div>
